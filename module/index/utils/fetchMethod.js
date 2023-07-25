191c46e7d769641b48c73e53e21331a70148315b
shreya-mishra <shreyamishra040398@gmail.com>
2023-07-25 20:38:17 +0530

    handle error

--- module/index/utils/fetchMethod.js
+++ module/index/utils/fetchMethod.js
@@ -1,16 +1,24 @@
 
 export const fetchMethod = async(api) => {
-    try{
-        const getPokemon = await fetch(api)
-        if(getPokemon.status != 200){
-            return 'something went wrong'
-        }
-        else{
-            const getPokemonJson = await getPokemon.json()
-            return getPokemonJson.results;
-    
-        }
-    }catch(error){
-        return error
-    }
+    try {
+        const getPokemon = await fetch(api);
+        getErrorStatus(getPokemon.status); // Added a function that can handle the error status efficiently.
+        const getPokemonJson = await getPokemon.json();
+        return Promise.resolve(getPokemonJson.results);
+      } catch (error) {
+        return Promise.reject(error);
+      }
 }
+
+const getErrorStatus = (status) => {
+    if (status === 401) {
+      throw new Error("Something went wrong"); // Instead of return we can directly throw error so it directly go to catch with some error message
+    }
+    if (status === 301) {
+      throw new Error("API is wrong"); // Instead of return we can directly throw error so it directly go to catch with some error message
+    }
+    if (status === 200) {
+      return null;
+    }
+    return null;
+  };
